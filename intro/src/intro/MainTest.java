f95417424443852ee31d0979337d529bc33a49ff
omeryilmaz2K <omeryilmaz2k@gmail.com>
2022-10-19 19:04:26 +0300

    Kullanıcı tarafından girilen 2 sayının toplamını bulan algoritma oluşturuldu.

--- intro/src/intro/MainTest.java
+++ intro/src/intro/MainTest.java
@@ -1,9 +1,14 @@
 package intro;
 
+import java.util.Scanner;
+
 public class MainTest {
+	
+	static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		sumOfTwoNumbersAlgorithm1();
+		// sumOfTwoNumbersAlgorithm1();
+		sumOfTwoNumbersAlgorithm2();
 	}
 
 	public static void sumOfTwoNumbersAlgorithm1() {
@@ -14,6 +19,14 @@ public class MainTest {
 		int sum = num1 + num2;
 		System.out.println(sum);
 		// System.out.println(num1 + num2);
+	}
+	
+	public static void sumOfTwoNumbersAlgorithm2() {
+		System.out.print("1. Sayı: ");
+		int num1 = scanner.nextInt();
+		System.out.print("2. Sayı: ");
+		int num2 = scanner.nextInt();
+		System.out.println("Sayıların toplamı: " + (num1 + num2));
 	}
 
 }
