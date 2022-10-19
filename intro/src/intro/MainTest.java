02c4bdbb80dce202e96aee1e63642b44945ccfed
omeryilmaz2K <omeryilmaz2k@gmail.com>
2022-10-19 19:00:15 +0300

    2 sayının toplamını bulan algoritma oluşturuldu.

--- intro/src/intro/MainTest.java
+++ intro/src/intro/MainTest.java
@@ -3,8 +3,17 @@ package intro;
 public class MainTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		sumOfTwoNumbersAlgorithm1();
+	}
 
+	public static void sumOfTwoNumbersAlgorithm1() {
+		// int num1 = 20;
+		byte num1 = 20;
+		byte num2 = 25;
+		// byte num1 = 20, num2 = 25;
+		int sum = num1 + num2;
+		System.out.println(sum);
+		// System.out.println(num1 + num2);
 	}
 
 }
