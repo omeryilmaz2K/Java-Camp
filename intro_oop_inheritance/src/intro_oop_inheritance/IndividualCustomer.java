79d9a440363b5a79811635b637ac848d5b5c0415
omeryilmaz2K <omeryilmaz2k@gmail.com>
2022-10-23 15:25:44 +0300

    Super Class - Base Class olarak Customer tanımlandı ve ortak alanlar oluşturularak, encapsulation gerçekleştirildi. Customer'dan inherit alan ve kendine ait alanları olan CorporateCustomer ve IndividualCustomer tanımlandı.

---
+++ intro_oop_inheritance/src/intro_oop_inheritance/IndividualCustomer.java
@@ -0,0 +1,26 @@
+package intro_oop_inheritance;
+
+public class IndividualCustomer extends Customer {
+	private String nationalIdentity;
+	private String firstName;
+	private String lastName;
+	
+	public String getNationalIdentity() {
+		return nationalIdentity;
+	}
+	public void setNationalIdentity(String nationalIdentity) {
+		this.nationalIdentity = nationalIdentity;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
--- intro_oop_inheritance/src/intro_oop_inheritance/Main.java
+++ intro_oop_inheritance/src/intro_oop_inheritance/Main.java
@@ -3,8 +3,17 @@ package intro_oop_inheritance;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		CorporateCustomer customer1 = new CorporateCustomer();
+		customer1.setId(0);
+		customer1.setCustomerNumber("231345");
+		customer1.setCompanyName("Hepsiburada");
+		customer1.setTaxNumber("3543433219");
+		IndividualCustomer customer2 = new IndividualCustomer();
+		customer2.setId(0);
+		customer2.setCustomerNumber("234365");
+		customer2.setNationalIdentity("48604381881");
+		customer2.setFirstName("Ömer");
+		customer2.setLastName("Yılmaz");
 	}
 
 }
